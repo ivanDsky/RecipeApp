72d5c15f0d20d2f2b58b8430320eef2990985b33
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-02-13 16:18:38 +0200

    Added viewBinding to BaseFragment

--- app/src/main/java/ua/zloydi/recipeapp/ui/core/BaseFragment.kt
+++ app/src/main/java/ua/zloydi/recipeapp/ui/core/BaseFragment.kt
@@ -1,8 +1,31 @@
 package ua.zloydi.recipeapp.ui.core
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment(@LayoutRes res: Int) : Fragment(res){
+abstract class BaseFragment<VB : ViewBinding>(@LayoutRes res: Int) : Fragment(res){
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = _binding ?: throw NullPointerException("View Binding is null")
 
+    abstract fun inflate(inflater: LayoutInflater) : VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate(inflater)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
