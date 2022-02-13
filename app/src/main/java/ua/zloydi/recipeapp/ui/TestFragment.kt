1b6e731640eccca41706365236658ec17a0ca871
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-02-13 18:16:44 +0200

    Changed test fragment

--- app/src/main/java/ua/zloydi/recipeapp/ui/TestFragment.kt
+++ app/src/main/java/ua/zloydi/recipeapp/ui/TestFragment.kt
@@ -1,10 +1,21 @@
 package ua.zloydi.recipeapp.ui
 
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import ua.zloydi.recipeapp.R
 import ua.zloydi.recipeapp.databinding.FragmentTestBinding
 import ua.zloydi.recipeapp.ui.core.BaseFragment
+import kotlin.random.Random
 
-class TestFragment : BaseFragment<FragmentTestBinding>(R.layout.fragment_test){
+class TestFragment : BaseFragment<FragmentTestBinding>(R.layout.fragment_test) {
     override fun inflate(inflater: LayoutInflater) = FragmentTestBinding.inflate(inflater)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvTitle.text = Random(System.currentTimeMillis())
+            .nextInt(10, 100)
+            .toString()
+        Log.d("Debug141", "onViewCreated: ${binding.tvTitle.text}")
+    }
 }
