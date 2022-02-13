dde31cc90c9e6ae6901703c1750f75b7c3e204df
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-02-13 17:48:54 +0200

    Added navigation to MainFragment

--- app/src/main/java/ua/zloydi/recipeapp/ui/main/MainFragment.kt
+++ app/src/main/java/ua/zloydi/recipeapp/ui/main/MainFragment.kt
@@ -3,15 +3,50 @@ package ua.zloydi.recipeapp.ui.main
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.commit
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
 import ua.zloydi.recipeapp.R
 import ua.zloydi.recipeapp.databinding.FragmentMainBinding
 import ua.zloydi.recipeapp.ui.core.BaseFragment
 
-class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main){
+class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
     override fun inflate(inflater: LayoutInflater) = FragmentMainBinding.inflate(inflater)
+
+    private val viewModel: MainFragmentViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupNavigation()
+        setupBottomNavigation()
     }
+
+    private fun setupNavigation() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.navigationScreenFlow.collect {
+                binding.toolbar.tvTitle.text = it.title
+                binding.bottomNavigation.selectedItemId = it.id
+                replaceFragment(it.fragment)
+            }
+        }
+    }
+
+    private fun setupBottomNavigation() {
+        with(binding.bottomNavigation) {
+            setOnItemSelectedListener {
+                viewModel.onMenuSelected(it)
+                return@setOnItemSelectedListener true
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        childFragmentManager.commit {
+            replace(R.id.mainContainer, fragment)
+        }
+    }
+
 }
