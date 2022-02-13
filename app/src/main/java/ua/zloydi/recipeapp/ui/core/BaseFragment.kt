b4119682918b00bb81beb0aae48afcdd33cc2025
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-02-13 14:44:34 +0200

    Created BaseFragment

---
+++ app/src/main/java/ua/zloydi/recipeapp/ui/core/BaseFragment.kt
@@ -0,0 +1,8 @@
+package ua.zloydi.recipeapp.ui.core
+
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment(@LayoutRes res: Int) : Fragment(res){
+
+}
