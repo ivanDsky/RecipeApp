379acbeb5761071a76a467c966907d5b872af1ce
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-02-13 14:41:25 +0200

    Initial commit

---
+++ app/src/main/java/ua/zloydi/recipeapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ua.zloydi.recipeapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.zloydi.recipeapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
