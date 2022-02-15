e76f5774e9898425c8f963bd7dfd7fe854c60290
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-02-15 09:27:40 +0200

    Added recycler view for SearchFragment

--- app/src/main/java/ua/zloydi/recipeapp/ui/search/SearchFragment.kt
+++ app/src/main/java/ua/zloydi/recipeapp/ui/search/SearchFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import ua.zloydi.recipeapp.R
 import ua.zloydi.recipeapp.databinding.FragmentSearchBinding
 import ua.zloydi.recipeapp.ui.core.BaseFragment
+import ua.zloydi.recipeapp.ui.core.adapter.RecipeAdapter
+import ua.zloydi.recipeapp.ui.core.adapterDecorators.PaddingDecoratorFactory
+import ua.zloydi.recipeapp.ui.core.adapterFingerprints.LongRecipeFingerprint
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(){
     private val viewModel: SearchFragmentViewModel by viewModels()
@@ -14,7 +18,17 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        with(binding.rvRecipes){
+            layoutManager = LinearLayoutManager(requireContext())
+            val adapter = RecipeAdapter(listOf(LongRecipeFingerprint()))
+            this.adapter = adapter
+            addItemDecoration(PaddingDecoratorFactory(resources).create(8f,20f,8f,0f))
+            adapter.setItems(viewModel.getItems(resources))
+        }
     }
 
 }
