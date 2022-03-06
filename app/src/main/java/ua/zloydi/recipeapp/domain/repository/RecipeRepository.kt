552f812b3d1f4d0cdd2973ded9e8fd565f537906
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-03-06 10:50:22 +0200

    Changed Repository according to errors

--- app/src/main/java/ua/zloydi/recipeapp/domain/repository/RecipeRepository.kt
+++ app/src/main/java/ua/zloydi/recipeapp/domain/repository/RecipeRepository.kt
@@ -1,10 +1,25 @@
 package ua.zloydi.recipeapp.domain.repository
 
+import ua.zloydi.recipeapp.data.dto.QueryDTO
+import ua.zloydi.recipeapp.data.errors.Error
+import ua.zloydi.recipeapp.domain.error.ErrorService
 import ua.zloydi.recipeapp.domain.retrofit.RecipeQuery
 import ua.zloydi.recipeapp.domain.retrofit.RetrofitService
 
 class RecipeRepository(
     private val retrofitService: RetrofitService,
+    private val errorService: ErrorService
 ) {
-    suspend fun query(query: RecipeQuery) = retrofitService.query(query)
+    suspend fun query(query: RecipeQuery) : QueryDTO?{
+        val response = retrofitService.query(query)
+        if(!response.isSuccessful){
+            errorService.submitError(Error.MessageError(response.errorBody()?.string() ?: "Response unknown error"))
+            return null
+        }
+        if(response.body() == null){
+            errorService.submitError(Error.MessageError("Empty response"))
+            return null
+        }
+        return response.body()
+    }
 }
