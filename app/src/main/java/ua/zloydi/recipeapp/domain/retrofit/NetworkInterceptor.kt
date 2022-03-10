97a3525c088dd6d53ff09fa6771eeae99422e32c
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-03-10 13:38:51 +0200

    Fixed NoInternetException should be IOException to catch by OkHttp

--- app/src/main/java/ua/zloydi/recipeapp/domain/retrofit/NetworkInterceptor.kt
+++ app/src/main/java/ua/zloydi/recipeapp/domain/retrofit/NetworkInterceptor.kt
@@ -3,11 +3,12 @@ package ua.zloydi.recipeapp.domain.retrofit
 import okhttp3.Interceptor
 import okhttp3.Response
 import ua.zloydi.recipeapp.utils.NetworkChecker
+import java.io.IOException
 
 class NetworkInterceptor(private val networkChecker: NetworkChecker) : Interceptor{
     override fun intercept(chain: Interceptor.Chain): Response {
         if(!networkChecker.checkConnection())
-            throw Exception("No internet connection")
+            throw IOException("No internet connection")
         return chain.proceed(chain.request())
     }
 }
