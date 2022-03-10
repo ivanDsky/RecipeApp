07852789c360ea5e1e556d5c7b2c8c093fc6ddd2
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-03-10 17:38:46 +0200

    Handle error messages in main activity

--- app/src/main/java/ua/zloydi/recipeapp/ui/MainActivity.kt
+++ app/src/main/java/ua/zloydi/recipeapp/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package ua.zloydi.recipeapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
 import ua.zloydi.recipeapp.R
+import ua.zloydi.recipeapp.domain.error.ErrorProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lifecycleScope.launchWhenStarted {
+            ErrorProvider.service.getErrors().receiveAsFlow().collect {
+                Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
