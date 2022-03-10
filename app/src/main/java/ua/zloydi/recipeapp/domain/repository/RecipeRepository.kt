5662ca88ef896c55e274348c7c68e61e8de6fcec
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-03-10 13:47:00 +0200

    Added try/catch to RecipeRepository

--- app/src/main/java/ua/zloydi/recipeapp/domain/repository/RecipeRepository.kt
+++ app/src/main/java/ua/zloydi/recipeapp/domain/repository/RecipeRepository.kt
@@ -5,21 +5,31 @@ import ua.zloydi.recipeapp.data.errors.Error
 import ua.zloydi.recipeapp.domain.error.ErrorService
 import ua.zloydi.recipeapp.domain.retrofit.RecipeQuery
 import ua.zloydi.recipeapp.domain.retrofit.RetrofitService
+import java.io.IOException
 
 class RecipeRepository(
     private val retrofitService: RetrofitService,
     private val errorService: ErrorService
 ) {
-    suspend fun query(query: RecipeQuery) : QueryDTO?{
-        val response = retrofitService.query(query)
-        if(!response.isSuccessful){
-            errorService.submitError(Error.MessageError(response.errorBody()?.string() ?: "Response unknown error"))
+    suspend fun query(query: RecipeQuery): QueryDTO? {
+        try {
+            val response = retrofitService.query(query)
+            if (!response.isSuccessful) {
+                submitErrorMessage(response.errorBody()?.string())
+                return null
+            }
+            if (response.body() == null) {
+                submitErrorMessage("Empty response")
+                return null
+            }
+            return response.body()
+        } catch (e: IOException) {
+            submitErrorMessage(e.message)
             return null
         }
-        if(response.body() == null){
-            errorService.submitError(Error.MessageError("Empty response"))
-            return null
-        }
-        return response.body()
+    }
+
+    private suspend fun submitErrorMessage(message: String?) {
+        errorService.submitError(Error.MessageError(message ?: "Response unknown error"))
     }
 }
