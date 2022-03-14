9a1dca8397062befc41fada94b7ddf9cd1a96b8c
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-03-14 15:12:53 +0200

    Changed DetailFragment

--- app/src/main/java/ua/zloydi/recipeapp/ui/detail/DetailFragmentViewModel.kt
+++ app/src/main/java/ua/zloydi/recipeapp/ui/detail/DetailFragmentViewModel.kt
@@ -1,6 +1,14 @@
 package ua.zloydi.recipeapp.ui.detail
 
 import androidx.lifecycle.ViewModel
+import ua.zloydi.recipeapp.data.ui.RecipeUI
 
 class DetailFragmentViewModel : ViewModel(){
+    private var _recipe: RecipeUI? = null
+    var recipe: RecipeUI
+        get() = if (_recipe == null) throw UninitializedPropertyAccessException() else _recipe!!
+        set(value) {
+            if (_recipe != null) return else _recipe = value
+        }
+
 }
