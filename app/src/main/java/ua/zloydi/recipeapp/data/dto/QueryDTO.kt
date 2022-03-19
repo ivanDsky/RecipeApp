4e69e4baee44316da6977d70936c3ab46c9cb912
zloydi <dobrovolskyivan2002@gmail.com>
2022-03-19 15:23:10 +0200

    Change API to support paging

--- app/src/main/java/ua/zloydi/recipeapp/data/dto/QueryDTO.kt
+++ app/src/main/java/ua/zloydi/recipeapp/data/dto/QueryDTO.kt
@@ -13,19 +13,10 @@ data class QueryDTO(
 
         other as QueryDTO
 
-        if (from != other.from) return false
-        if (to != other.to) return false
-        if (count != other.count) return false
-        if (_links != other._links) return false
-
-        return true
+        return _links != other._links
     }
 
     override fun hashCode(): Int {
-        var result = from
-        result = 31 * result + to
-        result = 31 * result + count
-        result = 31 * result + _links.hashCode()
-        return result
+        return _links.hashCode()
     }
 }
--- app/src/main/java/ua/zloydi/recipeapp/data/retrofit/RecipeApi.kt
+++ app/src/main/java/ua/zloydi/recipeapp/data/retrofit/RecipeApi.kt
@@ -11,6 +11,7 @@ interface RecipeApi {
     suspend fun queryFilters(
         @Query("q") query: String,
         @Query("field") fields: Array<String>,
+        @Query("_cont") nextHash: String? = null,
         @Query("cuisineType") cuisineType: String? = null,
         @Query("mealType") mealType: String? = null,
         @Query("dishType") dishType: String? = null,
