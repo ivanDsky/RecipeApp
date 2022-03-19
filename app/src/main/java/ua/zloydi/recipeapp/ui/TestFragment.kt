f60045da194c9e9f49e52c16f07a37fe0e4dc3f3
zloydi <dobrovolskyivan2002@gmail.com>
2022-03-19 15:23:47 +0200

    Change Test Fragment to pager adapter

--- app/src/main/java/ua/zloydi/recipeapp/ui/TestFragment.kt
+++ app/src/main/java/ua/zloydi/recipeapp/ui/TestFragment.kt
@@ -9,24 +9,23 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.transition.platform.MaterialContainerTransform
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.collect
 import ua.zloydi.recipeapp.R
 import ua.zloydi.recipeapp.data.filter_types.CuisineMapper
 import ua.zloydi.recipeapp.data.filter_types.DishMapper
 import ua.zloydi.recipeapp.data.filter_types.MealMapper
-import ua.zloydi.recipeapp.ui.data.IngredientUI
-import ua.zloydi.recipeapp.ui.data.RecipeItemUI
-import ua.zloydi.recipeapp.ui.data.RecipeUI
-import ua.zloydi.recipeapp.ui.data.filterType.CuisineUI
-import ua.zloydi.recipeapp.ui.data.filterType.DishUI
-import ua.zloydi.recipeapp.ui.data.filterType.MealUI
 import ua.zloydi.recipeapp.databinding.FragmentTestBinding
 import ua.zloydi.recipeapp.databinding.LayoutLongRecipeItemBinding
 import ua.zloydi.recipeapp.ui.core.BaseFragment
 import ua.zloydi.recipeapp.ui.core.adapter.baseAdapter.BaseAdapter
-import ua.zloydi.recipeapp.ui.core.adapter.recipeAdapter.RecipeAdapter
+import ua.zloydi.recipeapp.ui.core.adapter.recipeAdapter.RecipePagerAdapter
 import ua.zloydi.recipeapp.ui.core.adapterDecorators.PaddingDecoratorFactory
 import ua.zloydi.recipeapp.ui.core.adapterFingerprints.longRecipe.LongRecipeFingerprint
+import ua.zloydi.recipeapp.ui.data.IngredientUI
+import ua.zloydi.recipeapp.ui.data.RecipeUI
+import ua.zloydi.recipeapp.ui.data.filterType.CuisineUI
+import ua.zloydi.recipeapp.ui.data.filterType.DishUI
+import ua.zloydi.recipeapp.ui.data.filterType.MealUI
 import ua.zloydi.recipeapp.ui.detail.DetailFragment
 
 class TestFragment : BaseFragment<FragmentTestBinding>() {
@@ -39,43 +38,44 @@ class TestFragment : BaseFragment<FragmentTestBinding>() {
             drawingViewId = R.id.mainContainer
             duration
         }
-        lifecycleScope.launchWhenCreated {
-            setupAdapter(viewModel.uiRecipes.await())
-        }
+        setupAdapter()
     }
 
-
-    private fun setupAdapter(list: List<RecipeItemUI>) {
+    private fun setupAdapter() {
         with(binding.rvRecipes) {
             layoutManager = GridLayoutManager(requireContext(),2)
-            lateinit var adapter: RecipeAdapter
+            lateinit var adapter: RecipePagerAdapter
+            lifecycleScope.launchWhenStarted {
+                viewModel.flow.collect {
+                    adapter.submitData(it)
+                }
+            }
             val clickListener = BaseAdapter.OnItemClickListener { binding, position ->
                 val b = binding as LayoutLongRecipeItemBinding
-                val item = adapter.getItem(position)
+                val item = adapter.peek(position) ?: return@OnItemClickListener
                 parentFragmentManager.commit {
-                    val dto = runBlocking { viewModel.recipes.await() }?.find { it.image == item.image && it.label == item.title} ?: return@commit
-                    val ingredients = dto.ingredients?.map { IngredientUI(it.food, it.text, it.measure, it.image) }
+                    val ingredients = item.ingredients?.map { IngredientUI(it.food, it.text, it.measure, it.image) }
                     val cuisine = mutableListOf<CuisineUI>()
-                        dto.cuisineType?.forEach { it.split('/').forEach {CuisineMapper.enum(it)?.label?.let { cuisine.add(
+                        item.cuisineType?.forEach { it.split('/').forEach {CuisineMapper.enum(it)?.label?.let { cuisine.add(
                             CuisineUI(it)
                         ) }} }
                     val meal = mutableListOf<MealUI>()
-                        dto.mealType?.forEach { it.split('/').forEach {MealMapper.enum(it)?.label?.let { meal.add(
+                        item.mealType?.forEach { it.split('/').forEach {MealMapper.enum(it)?.label?.let { meal.add(
                             MealUI(it)
                         ) }} }
                     val dish = mutableListOf<DishUI>()
-                        dto.dishType?.forEach { it.split('/').forEach {DishMapper.enum(it)?.label?.let { dish.add(
+                        item.dishType?.forEach { it.split('/').forEach {DishMapper.enum(it)?.label?.let { dish.add(
                             DishUI(it)
                         ) }} }
                     val recipeUI = RecipeUI(
-                        dto.label,
-                        dto.image,
-                        dto.source,
+                        item.label,
+                        item.image,
+                        item.source,
                         null,
-                        dto.url,
+                        item.url,
                         ingredients?.toTypedArray(),
-                        dto.calories,
-                        dto.totalTime,
+                        item.calories,
+                        item.totalTime,
                         cuisine.toTypedArray(),
                         meal.toTypedArray(),
                         dish.toTypedArray()
@@ -86,10 +86,9 @@ class TestFragment : BaseFragment<FragmentTestBinding>() {
                     addToBackStack(null)
                 }
             }
-            adapter = RecipeAdapter(listOf(LongRecipeFingerprint),clickListener)
+            adapter = RecipePagerAdapter(listOf(LongRecipeFingerprint),clickListener)
             this.adapter = adapter
             PaddingDecoratorFactory(resources).apply(this, 8f, 8f)
-            adapter.setItems(list)
             doOnPreDraw { startPostponedEnterTransition() }
         }
     }
--- app/src/main/java/ua/zloydi/recipeapp/ui/TestFragmentVM.kt
+++ app/src/main/java/ua/zloydi/recipeapp/ui/TestFragmentVM.kt
@@ -2,23 +2,33 @@ package ua.zloydi.recipeapp.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import ua.zloydi.recipeapp.data.dto.RecipeDTO
+import ua.zloydi.recipeapp.data.error.ErrorProvider
 import ua.zloydi.recipeapp.data.filter_types.*
+import ua.zloydi.recipeapp.data.paging.RecipeSource
+import ua.zloydi.recipeapp.data.repository.RecipeRepository
+import ua.zloydi.recipeapp.data.retrofit.RecipeQuery
+import ua.zloydi.recipeapp.data.retrofit.RetrofitProvider
 import ua.zloydi.recipeapp.ui.data.RecipeItemUI
 import ua.zloydi.recipeapp.ui.data.filterType.CuisineUI
 import ua.zloydi.recipeapp.ui.data.filterType.DishUI
 import ua.zloydi.recipeapp.ui.data.filterType.MealUI
-import ua.zloydi.recipeapp.data.error.ErrorProvider
-import ua.zloydi.recipeapp.data.repository.RecipeRepository
-import ua.zloydi.recipeapp.data.retrofit.RecipeQuery
-import ua.zloydi.recipeapp.data.retrofit.RetrofitProvider
 
 class TestFragmentVM : ViewModel(){
+    private val repository = RecipeRepository(RetrofitProvider.service, ErrorProvider.service)
+
+    val pager = Pager(PagingConfig(20,20,false,40),null){
+        RecipeSource(repository,RecipeQuery("", cuisineType = Cuisine.CentralEurope))
+    }
+    val flow = pager.flow.cachedIn(viewModelScope)
+
     val recipes: Deferred<List<RecipeDTO>?> = viewModelScope.async(Dispatchers.IO) {
-        val repository = RecipeRepository(RetrofitProvider.service, ErrorProvider.service)
         repository.query(RecipeQuery("", cuisineType = Cuisine.CentralEurope))?.let { query ->
             query.hits?.map { it.recipe }
         }
